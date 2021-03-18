d260beaab92cc08a4460364ec474768870b986c8
Sanches <autoscript@gmail.com>
2021-03-18 05:00:39 +0300

    00000

--- Program.cs
+++ Program.cs
@@ -4,34 +4,43 @@ namespace HomeWork5
 {
     class Program
     {
+        //public interface ITask
+        //{
+
+        //}
         static void Main(string[] args)
         {
             //I5.5.Мастям игральных карт условно присвоены следующие порядковые номера:
             //масти «пики» — 1, масти «трефы» — 2, масти «бубны» — 3, масти «червы» — 4.
             //По заданному номеру масти m(1 <= m <= 4) определить название соответствующей масти.
 
-            int correctInput = 0;
-            string output;
-
             Console.WriteLine("1st. I5.5");
+            
+            string output;
+            int correctInput = KeyInput();
+            new GetFromEnum(correctInput - 1, out output); //инициализируем(??) экземпляр и кидаем туда ввод от пользователя
+            Console.WriteLine($"\nCorresponding to index {correctInput} suit is {output}");
+            Console.ReadKey();
+        }
+            public static int KeyInput()
+            {
+            int correctInput2;
             Console.Write("\nEnter number (1-4) to search the corresponded suit: ");
-
             for (; ; )
             {
-                var userInput = Console.ReadKey();
-                if (char.IsDigit(userInput.KeyChar)) //проверяем чар на циферность
+                var inputKey = Console.ReadKey();
+                if (char.IsDigit(inputKey.KeyChar)) //проверяем чар на циферность
                 {
-                    correctInput = int.Parse(userInput.KeyChar.ToString());//кастуем к инту
-                    if(correctInput is >0 and <5)// если не выставить совместимость с Net.5 не работает
+                    correctInput2 = int.Parse(inputKey.KeyChar.ToString());//кастуем к инту
+                    if (correctInput2 is (> 0 and < 5))
                     { break; }
                 }
                 Console.WriteLine("\nError. Retype please");
             }
-            new TaskI55(correctInput-1, out output); //инициализируем(??) экземпляр и кидаем туда ввод от пользователя
-            Console.WriteLine($"\nCorresponding to index {correctInput} suit is {output}");
-            Console.ReadKey();
-    }
-    public class TaskI55 
+            return correctInput2;
+            }
+
+        public class GetFromEnum 
         {
             private enum Suits // инкапсулируем
             {
@@ -40,13 +49,13 @@ namespace HomeWork5
                 Diamonds,
                 Hearts,
             }
-            public TaskI55(int input, out string output) //принимаем инт и выкидываем стринг из перечисления
+            public GetFromEnum(int input, out string output) //принимаем инт и выкидываем стринг из перечисления
             {
-                //output = ((Suits)input).ToString; не хочет
                 var suit = (Suits)Enum.GetValues(typeof(Suits)).GetValue(input); //спиздил кусок кода
                 output = suit.ToString();
                 return;
             }
         }
+        
     }
 }
