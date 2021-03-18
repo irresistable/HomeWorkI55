c0388b640abe681d6b2ea314a08f1a671d4f2841
Sanches <autoscript@gmail.com>
2021-03-18 18:20:37 +0300

    000000

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@ namespace HomeWork5
 {
     class Program
     {
-        //public interface ITask
-        //{
-
-        //}
         static void Main(string[] args)
         {
             //I5.5.Мастям игральных карт условно присвоены следующие порядковые номера:
@@ -15,36 +11,20 @@ namespace HomeWork5
             //По заданному номеру масти m(1 <= m <= 4) определить название соответствующей масти.
 
             Console.WriteLine("1st. I5.5");
-            
+            int value;
             string output;
-            int correctInput = KeyInput();
-            new GetFromEnum(correctInput - 1, out output); //инициализируем(??) экземпляр и кидаем туда ввод от пользователя
-            Console.WriteLine($"\nCorresponding to index {correctInput} suit is {output}");
+            string name = "suit"; //тут храним описание существительного
+
+            new UserInput(1, 4, name, out value);
+            new GetFromEnum(value - 1, out output); //инициализируем(??) экземпляр и кидаем туда ввод от пользователя
+            Console.WriteLine($"\nCorresponding to index {value} {name} is {output}");
             Console.ReadKey();
         }
-            public static int KeyInput()
-            {
-            int correctInput2;
-            Console.Write("\nEnter number (1-4) to search the corresponded suit: ");
-            for (; ; )
-            {
-                var inputKey = Console.ReadKey();
-                if (char.IsDigit(inputKey.KeyChar)) //проверяем чар на циферность
-                {
-                    correctInput2 = int.Parse(inputKey.KeyChar.ToString());//кастуем к инту
-                    if (correctInput2 is (> 0 and < 5))
-                    { break; }
-                }
-                Console.WriteLine("\nError. Retype please");
-            }
-            return correctInput2;
-            }
-
-        public class GetFromEnum 
+        public class GetFromEnum
         {
             private enum Suits // инкапсулируем
             {
-                Spades=1, // иначе индекс будет с 0  НО ПОЧЕМУ ТО НЕ РАБОТАЕТ ТАК, ПРИШЛОСЬ -1 ДЕЛАТЬ В ПЕРЕДАЧЕ ЗНАЧЕНИЯ
+                Spades = 1, // иначе индекс будет с 0  НО ПОЧЕМУ ТО НЕ РАБОТАЕТ ТАК, ПРИШЛОСЬ -1 ДЕЛАТЬ В ПЕРЕДАЧЕ ЗНАЧЕНИЯ
                 Clubs,
                 Diamonds,
                 Hearts,
@@ -56,6 +36,33 @@ namespace HomeWork5
                 return;
             }
         }
-        
+        public class UserInput 
+            {
+            int _minValue = 0;
+            int _maxValue = 9;
+            string _name = "";
+        public UserInput(int minValue, int maxValue, string name, out int UserInput)// принимаем лимиты ввода и возвращаем проверенный ввод
+        {
+                _minValue = minValue;
+                _maxValue = maxValue;
+                _name = name;
+                int UserInput2;
+
+                Console.Write($"\nEnter number ({minValue}-{maxValue}) to search the corresponded {_name}: ");
+                for (; ; )
+                {
+                    var inputKey = Console.ReadKey();
+                    if (char.IsDigit(inputKey.KeyChar)) //проверяем чар на циферность
+                    {
+                        UserInput2 = int.Parse(inputKey.KeyChar.ToString());//кастуем к инту
+                        if ((UserInput2 >= _minValue) && (UserInput2 <= _maxValue))
+                            { break; }
+                    }
+                    Console.WriteLine("\nError. Retype please");
+                }
+                UserInput = UserInput2;
+                return;
+            }
     }
+}
 }
